fc7437907beecb344a2826002dc8193b95ef3c14
Aqq-dev <ryuryur330@gmail.com>
2025-08-29 07:27:16 +0900

    Create ninFlaskV7.py

---
+++ ninFlaskV7.py
@@ -0,0 +1,42 @@
+from threading import Thread
+from flask import Flask, request
+import os
+import db
+from asyncEAGM import EAGM, EAGMError
+
+app = Flask("app")
+BOTTOKEN=os.getenv("BOTTOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+eagm=EAGM(bot_token=BOTTOKEN,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI)
+
+@app.route("/", methods=["GET"])
+async def index():
+    try:
+        code = request.args.get("code", "")
+        if not code:
+            return
+        state = request.args.get("state", "").split("=")
+        server_id = str(int(state[0],16))
+        server_data = db.get_server(server_id)
+        if not server_data:
+            return "<h1>サーバーデータが見つかりません</h1>",400
+
+        try:
+            await eagm.get_token(code)
+            await eagm.get_user(eagm.access_token)
+        except EAGMError:
+            return "<h1>400: invalid scope</h1>",400
+        
+        addrole=await eagm.add_role(user_id=eagm.user_id,guild_id=server_id,role_id=server_data["role_id"])
+        if addrole!=204:
+            return "<h1>ロール付与に失敗しました</h1>",400
+        
+        db.upsert_user(eagm.user_id, eagm.access_token)
+
+    except Exception as e:
+        return f"<h1>エラー : {e}</h1>",500
+
+    return f"<h1>登録成功！ {eagm.global_name}さんよろしく！</h1>"
